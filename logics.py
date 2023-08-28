78960e43e70cd9e981f7c9d7e564b61c16e800ee
w3sm1l3 <cool.makowka@ya.ru>
2023-08-29 00:14:22 +0700

    v_alpha_0.06 add knight logic add bishop logic

--- logics.py
+++ logics.py
@@ -37,8 +37,8 @@ def print_lg_board(board):
     print("")
 
 def pawn_corner_check(x, y):
-    corner_x_dict = {7: "U", 0: "D"}
-    corner_y_dict = {7: "R", 0: "R"}
+    corner_x_dict = {0: "U", 8: "D"}
+    corner_y_dict = {7: "R", 0: "L"}
 
     if x in list(corner_x_dict.keys()):
         return corner_x_dict[x]
@@ -82,13 +82,84 @@ def rook_logic(board, color, x, y):
     """Создание массива возможных ходов ладьи"""
     pass
 
+def knight_corner_check(knight_moves):
+    moves = []
+    for move in knight_moves:
+        if move[0] in [0, 1, 2, 3, 4, 5, 6, 7] and move[1] in [0, 1, 2, 3, 4, 5, 6, 7]:
+            moves.append(move)
+
+    return moves
+
+
 def knight_logic(board, color, x, y):
     """Создание массива возможных ходов коня"""
-    pass
+    knight_moves = [[x-2, y+1], [x-1, y+2], [x+1, y+2], [x+2, y+1], [x+2, y-1], [x+1, y-2], [x-1, y-2], [x-2, y-1]]
+    possible_moves = []
+    knight_moves = knight_corner_check(knight_moves)
+
+    if color == White:
+        for move in knight_moves:
+            x = move[0]
+            y = move[1]
+            if not board[x][y] or board[x][y].color == Black:
+                possible_moves.append(move)
+    else:
+        for move in knight_moves:
+            x = move[0]
+            y = move[1]
+            if not board[x][y] or board[x][y].color == White:
+                possible_moves.append(move)
+
+    return possible_moves
 
 def bishop_logic(board, color, x, y):
     """Создание массива возможных ходов слона"""
-    pass
+    possible_moves = []
+    bishop_moves_ur = []
+    bishop_moves_lr = []
+    bishop_moves_ul = []
+    bishop_moves_ll = []
+    bishop_moves_all = [bishop_moves_ur, bishop_moves_lr, bishop_moves_ul, bishop_moves_ll]
+
+    for i, j, k in zip(range(x, 0, -1), range(y, 7, 1), range(1, 8, 1)):
+        bishop_moves_ur.append([x - k, y + k])
+
+    for i, j, k in zip(range(x, 7, 1), range(y, 7, 1), range(1, 8, 1)):
+        bishop_moves_lr.append([x + k, y + k])
+
+    for i, j, k in zip(range(x, 7, 1), range(y, 0, -1), range(1, 8, 1)):
+        bishop_moves_ll.append([x + k, y - k])
+
+    for i, j, k in zip(range(x, 0, -1), range(y, 0, -1), range(1, 8, 1)):
+        bishop_moves_ul.append([x - k, y - k])
+
+    if color == White:
+        for bishop_moves in bishop_moves_all:
+            for move in bishop_moves:
+                x = move[0]
+                y = move[1]
+                if not board[x][y]:
+                    possible_moves.append(move)
+                elif board[x][y].color == Black:
+                    possible_moves.append(move)
+                    break
+                elif board[x][y].color == White:
+                    break
+
+    else:
+        for bishop_moves in bishop_moves_all:
+            for move in bishop_moves:
+                x = move[0]
+                y = move[1]
+                if not board[x][y]:
+                    possible_moves.append(move)
+                elif board[x][y].color == White:
+                    possible_moves.append(move)
+                    break
+                elif board[x][y].color == Black:
+                    break
+
+    return possible_moves
 
 def queen_logic(board, color, x, y):
     """Создание массива возможных ходов королевы"""
